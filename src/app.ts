dc7c22b8fe1b3fc027e8cf978619b59940d52a6d
FrankRay78 <52075808+FrankRay78@users.noreply.github.com>
2021-01-14 19:32:48 +0000

    Split out apple TS class into a separate module which is then imported

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,5 @@
 ﻿
-class Apple {
-
-    //nb. zero based
-    public currentX: number;
-    public currentY: number;
-
-    get Position() {
-        return { currentX: this.currentX, currentY: this.currentY };
-    }
-
-    constructor(private startingX: number, private startingY: number) {
-        this.currentX = startingX;
-        this.currentY = startingY;
-    }
-
-    Initialise(): void {
-        this.currentX = this.startingX;
-        this.currentY = this.startingY;
-    }
-}
+import Apple = require('./models/apple');
 
 enum SnakeDirection {
     Up = 1,
---
+++ src/models/apple.ts
@@ -0,0 +1,28 @@
+﻿
+//https://stackoverflow.com/questions/35405412/how-to-use-npm-installed-requirejs-for-browser
+//https://stackoverflow.com/questions/20079464/whats-the-correct-way-to-use-requirejs-with-typescript
+//https://requirejs.org/docs/start.html#examples
+//https://devblogs.microsoft.com/visualstudio/using-requirejs-with-visual-studio/
+
+class Apple {
+
+    //nb. zero based
+    public currentX: number;
+    public currentY: number;
+
+    get Position() {
+        return { currentX: this.currentX, currentY: this.currentY };
+    }
+
+    constructor(private startingX: number, private startingY: number) {
+        this.currentX = startingX;
+        this.currentY = startingY;
+    }
+
+    Initialise(): void {
+        this.currentX = this.startingX;
+        this.currentY = this.startingY;
+    }
+}
+
+export = Apple;
