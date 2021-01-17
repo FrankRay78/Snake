2b51f16617f04fafc12bc6838ac7ba43282224a1
FrankRay78 <52075808+FrankRay78@users.noreply.github.com>
2021-01-17 22:11:08 +0000

    Added Apple again (changed from lowercase apple.js filename)

---
+++ scripts/models/Apple.js
@@ -0,0 +1,23 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var Apple = /** @class */ (function () {
+        function Apple(startingX, startingY) {
+            this.startingX = startingX;
+            this.startingY = startingY;
+        }
+        Object.defineProperty(Apple.prototype, "Position", {
+            get: function () {
+                return { currentX: this.currentX, currentY: this.currentY };
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Apple.prototype.Initialise = function () {
+            this.currentX = this.startingX;
+            this.currentY = this.startingY;
+        };
+        return Apple;
+    }());
+    return Apple;
+});
+//# sourceMappingURL=Apple.js.map
