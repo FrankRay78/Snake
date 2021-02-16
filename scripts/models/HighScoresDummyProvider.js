7eb49d97bb0bcdff3c1ed4867dbc149596eaa73e
FrankRay78 <52075808+FrankRay78@users.noreply.github.com>
2021-02-16 12:00:08 +0000

    Fully implemented (TODO: HighScoresServiceProvider)

--- scripts/models/HighScoresDummyProvider.js
+++ scripts/models/HighScoresDummyProvider.js
@@ -5,30 +5,54 @@ define(["require", "exports", "./HighScore"], function (require, exports, HighSc
      */
     class HighScoresDummyProvider {
         constructor() {
-            this.highScores = [];
-            ////Pre-seeds
-            //this.highScores.push(new HighScore("FDR", 10));
-            //this.highScores.push(new HighScore("HGR", 7));
-            //this.highScores.push(new HighScore("SRR", 2));
+            this.scores = [];
+            //Pre-seeds
+            this.scores.push(new HighScore("FDR", 10));
+            this.scores.push(new HighScore("HGR", 7));
+            this.scores.push(new HighScore("SRR", 2));
+            //this.scores.push(new HighScore("A", 1));
+            //this.scores.push(new HighScore("B", 1));
+            //this.scores.push(new HighScore("C", 1));
+            //this.scores.push(new HighScore("D", 1));
+            //this.scores.push(new HighScore("E", 1));
+            //this.scores.push(new HighScore("F", 1));
+            //this.scores.push(new HighScore("G", 1));
         }
         GetHighScores() {
             //Return the top 10 scores
-            return this.highScores.sort((a, b) => {
+            return this.scores.sort((a, b) => {
                 return b.PlayerScore - a.PlayerScore;
-            }).slice(0, 9);
+            }).slice(0, 10);
         }
         ;
         IsHighScore(score) {
-            //Accept all scores greater than 0
-            return (score && score > 0);
+            if (!score || score <= 0)
+                return false;
+            const highScores = this.GetHighScores();
+            if (highScores.length < 10)
+                return true;
+            //Check if the high score really does cut it
+            if (score > highScores[highScores.length - 1].PlayerScore)
+                return true;
+            return false;
         }
         ;
         SaveHighScore(initials, score) {
-            if (this.IsHighScore(score) &&
-                initials && initials.length > 0 &&
-                initials.length < 4) {
-                //Valid initials and score
-                this.highScores.push(new HighScore(initials.toUpperCase(), score));
+            if (this.IsHighScore(score)) {
+                if (initials && initials.length > 0 && initials.length < 4) {
+                    //Initials and score are valid
+                    initials = initials.toUpperCase();
+                    //Make sure the initials and score aren't already in the high scores
+                    const highScores = this.GetHighScores();
+                    for (let i = 0; i < highScores.length; i++) {
+                        const highScore = highScores[i];
+                        if (highScore.PlayerInitials === initials && highScore.PlayerScore === score) {
+                            //Already exists so return
+                            return;
+                        }
+                    }
+                    this.scores.push(new HighScore(initials, score));
+                }
             }
         }
         ;
